b3bdf119f0959464b5c341e7e5b23eda8a3681d0
Quan <urjhams@gmail.com>
2018-04-15 13:33:38 +0200

    2a: success in asking permission for location service, success in get current position

--- app/src/main/java/com/example/mis/polygons/MapsActivity.java
+++ app/src/main/java/com/example/mis/polygons/MapsActivity.java
@@ -10,6 +10,8 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -41,25 +43,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        LocationManager mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(this,
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        Location current = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        LatLng currentLatlng = new LatLng(current.getLatitude(), current.getLongitude());
-        mMap.addMarker(new MarkerOptions().position(currentLatlng).title("You are here"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatlng));
+        mMap.setBuildingsEnabled(true);
+        this.setMarkerOf(deviceCurrentLocation(),"You are here my friend", mMap);
     }
 
     private boolean isGrantedPermission() {
@@ -73,15 +58,58 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 MY_PERMISSIONS_REQUEST_READ_CONTACTS);
     }
 
+    private void setMarkerOf(Location location, String title, GoogleMap map) {
+        LatLng currentLatlng = new LatLng(location.getLatitude(), location.getLongitude());
+        map.addMarker(new MarkerOptions().position(currentLatlng).title(title));
+        //move camera to new marked with zoom level 15
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatlng, 15.0f));
+        //zoom in to map til level 12
+        map.animateCamera(CameraUpdateFactory.zoomIn());
+        //map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatlng,12.0f));
+        map.animateCamera(CameraUpdateFactory.zoomTo(12.0f),2000,null);
+    }
+
+    private Location deviceCurrentLocation() {
+        LocationManager mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Location defaultLocation = new Location("Weimar, Thuringia, Germany");
+            defaultLocation.setLatitude(50.979492);
+            defaultLocation.setLongitude(11.323544);
+            return defaultLocation;
+        } else {
+            //get current latitude and longitude
+            return mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+    }
+
+    private void makeToast(String content, Context context) {
+        Toast toast = Toast.makeText(context,content,Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER,0,0);
+        toast.show();
+    }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == MY_PERMISSIONS_REQUEST_READ_CONTACTS) {
             switch (requestCode) {
                 case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
                     if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED ) {
-                        //granted
+                        // permission granted
+                        Location current = deviceCurrentLocation();
+                        this.setMarkerOf(current,"You are here my friend", mMap);
                     } else {
-                        //not granted
+                        // not granted - permission denied
+                        this.makeToast("Cannot located device, set default place to Weimar",this);
                     }
                     return;
                 }
